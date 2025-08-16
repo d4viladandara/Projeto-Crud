d7f451a992cb9310392e4e36c9854111f06e7870
D'Ávila Dandara <d.dandara@academico.ifpb.edu.br>
2025-08-16 14:17:17 -0300

    criando o banco de dados para categorias.html

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meubanco.db'
+db = SQLAlchemy(app)
+
+# definindo o modelo de dados
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), unique=True, nullable=False)
+
+
+#cRud base (fazer depoissss)
+@app.route('/base')
 def index():
     return render_template('base.html')
+
+#cRud categorias 
+@app.route('/')
+def categorias():
+    tasks = Tasks.query.all()
+    return render_template('categorias.html', tasks=tasks)
+
 if __name__ =='__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
