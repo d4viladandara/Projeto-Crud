16d0f20bf8236b8a78a394f5e15e8eb5a2044bda
D'Ávila Dandara <d.dandara@academico.ifpb.edu.br>
2025-08-16 19:06:03 -0300

    criando crud de categorias

--- app.py
+++ app.py
@@ -21,7 +21,41 @@ def index():
 @app.route('/categorias')
 def categorias():
     tasks = Tasks.query.all()
-    return render_template('categorias.html', tasks=tasks)
+    return render_template('/categorias.html', tasks=tasks)
+
+@app.route('/create', methods=['POST'])
+def create_task():
+    description = request.form['description']
+    new_task = Tasks(description=description)
+    
+    existe_task = Tasks.query.filter_by(description=description).first()
+    if existe_task:
+        return 'Erro: Produto já cadastrado'
+    
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/categorias')
+
+# Para exluir da lista
+@app.route('/delete/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task = Tasks.query.get(task_id)
+    
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect('/categorias')
+
+    
+# Para editar a lista (update)
+@app.route('/update/<int:task_id>', methods=["POST"])
+def update_task(task_id):
+    task = Tasks.query.get(task_id)
+    
+    if task: 
+        task.description = request.form['description']
+        db.session.commit()
+    return redirect('/categorias')
 
 if __name__ =='__main__':
     with app.app_context():
